c611ad6ae35022b63b89e95446ddf9aa37fa9e75
Pat Ugarte <patriciomugarte@gmail.com>
2019-11-14 03:43:42 -0300

    Generado el enrutamiento de la sección Contacto.

--- index.js
+++ index.js
@@ -261,7 +261,13 @@ ${getStringSucursales(theaters)}
 `);
 			break;
 		case '/contacto':
-			res.end('Contacto');
+			res.end(`CONTACTO
+
+Contáctanos:
+¿Tenés algo para contarnos? Nos encanta escuchar a nuestros clientes. Si deseas contactarnos podés escribirnos al siguiente email:	dhmovies@digitalhouse.com	o en nuestras redes sociales @dhmoviesOK. 
+
+Envianos tu consulta, sugerencia o reclamo y será respondido a la brevedad. Recordá que también podes consultar la sección de Preguntas Frecuentes para obtener respuestas inmediatas a los problemas más comunes.
+`);
 			break;
 		case '/preguntas-frecuentes':
 			res.end('Preguntas Frecuentes');
